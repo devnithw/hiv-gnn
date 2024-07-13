ceb513dc24f6817f05ce5f03f75877818eb3faa3
devnithw <devnithw@gmail.com>
2024-07-13 09:41:24 +0530

    Added new model

--- model.py
+++ model.py
@@ -2,11 +2,12 @@ import torch
 import torch.nn.functional as F
 from torch.nn import Sequential, Linear, BatchNorm1d, ReLU
 from torch_geometric.nn import TransformerConv, GATConv, TopKPooling, BatchNorm
+from torch_geometric.nn import GCNConv
 from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 from torch_geometric.nn.conv.x_conv import XConv
 torch.manual_seed(42)
 
-class GNN(torch.nn.Module):
+class GATNetwork(torch.nn.Module):
     def __init__(self, feature_size):
         """
         Graph Neural Network model made with Graph Attention layers and TopK pooling.
@@ -15,7 +16,7 @@ class GNN(torch.nn.Module):
         Parameters - feature_size
 
         """
-        super(GNN, self).__init__()
+        super(GATNetwork, self).__init__()
         
         # Hardcoded parameters
         num_classes = 2
@@ -65,5 +66,44 @@ class GNN(torch.nn.Module):
         return x
 
         
+class GCNConvNetwork(torch.nn.Module):
+    def __init__(self, feature_size):
+        """
+        Graph Neural Network model made with GCN Convolution Layers
+        Consists of three layers of GCNConv layers, followed by two Linear layers.
+        Output is two classes.
+        Parameters - feature_size
 
+        """
+        super(GATNetwork, self).__init__()
+        
+        # Hardcoded parameters
+        num_classes = 2
+        hidden_channels = 512
+
+        # Layers
+        self.conv1 = GCNConv(feature_size, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, hidden_channels)
+        self.linear1 = Linear(hidden_channels, 64)
+        self.linear2 = Linear(64, num_classes)
+
+
+    def forward(self, x, edge_attr, edge_index, batch_index):
+        #GCN block (Obtain node embeddings)
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x, edge_index)
+        x = x.relu()
+        x = self.conv3(x, edge_index)
+
+        # Readout layer
+        x = gap(x, batch_index)
+        
+        # Linear block
+        x = self.linear1(x).relu()
+        x = F.dropout(x, p=0.5, training=self.training)
+        x = self.linear2(x)
+
+        return x
 
